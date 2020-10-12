b467e411d8ba6732441ae77465741a2bf221d003
taishikato <taishi.k0903@gmail.com>
2020-10-11 18:53:54 -0700

    use env for hero subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,11 +14,7 @@ const Hero = () => {
         />
         <div className="text-center lg:w-2/3 w-full">
           <H1>{process.env.NEXT_PUBLIC_HERO_TITLE}</H1>
-          <p className="mb-8 leading-relaxed">
-            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust
-            fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over
-            meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.
-          </p>
+          <p className="mb-8 leading-relaxed">{process.env.NEXT_PUBLIC_HERO_SUBTITLE}</p>
         </div>
       </div>
     </section>
